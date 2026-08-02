a0be53d59886574fea4df4a30b865f57e2c68c4a
Adel Muhammad <dolaadel574@gmail.com>
2026-08-02 18:02:17 +0300

    change api to jsonData

--- src/assets/components/ProductDescription/ProImage.jsx
+++ src/assets/components/ProductDescription/ProImage.jsx
@@ -1,20 +1,16 @@
-import React, { useRef } from 'react'
+import { useRef } from "react";
 
-import img from "../../images/Blazer/one/3f5847bf-6d02-4f17-ae0d-9fc1f3405c7e-removebg-preview_440x466.webp"
-import img1 from "../../images/Suit/1/07c0dcfc-2b19-470e-aade-d7fb84f1a9d7-removebg-preview_440x466.webp"
-
-function ProImage({number , product}) {
-
-  const z = useRef(null)
-  const h = useRef(null)
+import img from "../../images/Blazer/one/3f5847bf-6d02-4f17-ae0d-9fc1f3405c7e-removebg-preview_440x466.webp";
+function ProImage({ product }) {
+  const z = useRef(null);
+  const h = useRef(null);
 
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> img move
-  function imgMove (e) {
-
-    const rect = z.current.getBoundingClientRect();  // بتجيب ابعاد المكان الى هتتحرك فى الصورة
+  function imgMove(e) {
+    const rect = z.current.getBoundingClientRect(); // بتجيب ابعاد المكان الى هتتحرك فى الصورة
 
     const x = e.clientX - rect.left; // Mouse X relative to container
-    const y = e.clientY - rect.top;  // Mouse Y relative to container
+    const y = e.clientY - rect.top; // Mouse Y relative to container
 
     const offsetX = (x / rect.width - 0.5) * -100; // عكس الاتجاه
     const offsetY = (y / rect.height - 0.5) * -100;
@@ -23,46 +19,50 @@ function ProImage({number , product}) {
   }
 
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> return img to default location
-  function imgLeave () {
+  function imgLeave() {
     h.current.style.transform = `translate(0px, 0px)`;
   }
 
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Scale Img
-  function imgScale () {
-    h.current.style.transform = "scale(1.2)" ;
+  function imgScale() {
+    h.current.style.transform = "scale(1.2)";
   }
 
-  function chanegePhoto (e) {
-
+  function chanegePhoto(e) {
     const newSrc = e.target.getAttribute("src");
-    h.current.setAttribute("src" , newSrc)
-    
+    h.current.setAttribute("src", newSrc);
   }
-
 
   return (
-    <div className='ProImage'>
-
-      <div ref={z} className='mainImg' onMouseMove={(e)=> imgMove(e)} onMouseLeave={imgLeave} onClick={imgScale}>
-        <img ref={h} src= {img} alt="" />
+    <div className="ProImage">
+      <div
+        ref={z}
+        className="mainImg"
+        onMouseMove={(e) => imgMove(e)}
+        onMouseLeave={imgLeave}
+        onClick={imgScale}>
+        <img ref={h} src={img} alt="" />
         {/* <img ref={h} src= {`/src/assets/${product?.images[0].split("/").splice(3).join("/")}`} alt="" /> */}
       </div>
 
       <div className="selectImg">
-
-        {product.images.map((el,index)=> {
+        {product.images.map((el, index) => {
           return (
-            <div key={index} >
+            <div key={index}>
               {console.log(el)}
-              <img onClick={(e)=> chanegePhoto(e)} src= {el} className='w-100 h-100' alt="" />
+              <img
+                onClick={(e) => chanegePhoto(e)}
+                src={el}
+                className="w-100 h-100"
+                alt=""
+              />
               {/* <img onClick={(e)=> chanegePhoto(e)} src= {`/src/assets/${el.split("/").splice(3).join("/")}`} className='w-100 h-100' alt="" /> */}
             </div>
-          )
+          );
         })}
-
       </div>
     </div>
-  )
+  );
 }
 
-export default ProImage
+export default ProImage;
--- src/assets/components/ProductDescription/ProductDescription.jsx
+++ src/assets/components/ProductDescription/ProductDescription.jsx
@@ -1,44 +1,36 @@
-import { useParams } from "react-router-dom"
-import SecEleven from "../Home/SecEleven/SecEleven"
-import SecThree from "../Home/SecThree/SecThree"
-import CustomerReview from "./CustomerReview"
-import ProDesc from "./proDesc"
-import "./ProductDescription.css"
-import ProImage from "./ProImage.jsx"
-import { useContext } from "react"
-import { productContext } from "../../../Context/ProductContext"
+import { useParams } from "react-router-dom";
+import SecEleven from "../Home/SecEleven/SecEleven.jsx";
+import CustomerReview from "./CustomerReview.jsx";
+import ProDesc from "./proDesc.jsx";
+import "./ProductDescription.css";
+import ProImage from "./ProImage.jsx";
+import { useContext } from "react";
+import { productContext } from "../../../Context/ProductContext.jsx";
 
 function ProductDescription() {
+  const { number } = useParams();
 
-  const {number} = useParams()
-
-  const {product} = useContext(productContext)
-
-
-
+  const { product } = useContext(productContext);
 
   return (
     <div className="ProductDescription">
       <div className="main">
         <div className="image p-4">
-          <ProImage product= {product} number= {number} />
+          <ProImage product={product} />
         </div>
 
         <div className="description p-4">
-          <ProDesc product= {product} number= {number} />
+          <ProDesc product={product} />
         </div>
-
       </div>
-
 
       <div className="my-5 py-5">
         <SecEleven />
       </div>
 
       <CustomerReview />
-      
     </div>
-  )
+  );
 }
 
-export default ProductDescription
+export default ProductDescription;
